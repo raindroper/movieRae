efe32c110e1840248099663014f2bd267dbb792d
Rae <houxiaoyu1993@126.com>
2017-09-18 14:15:16 +0800

    回滚 =>无轮播图

--- home/home.js
+++ home/home.js
@@ -29,14 +29,4 @@
       $scope.$apply();
     })
   }])
-  app.controller('PostListController',
-      function ($scope) {
-        // 设置轮播图图片间隔
-        $scope.myInterval = 5000;
-        // 轮播图数据初始化
-        var slides = $scope.slides = [];
-        // 添加轮播图源
-        slides.push({ image: './assets/img/aizaishenqiu.jpg', text: '亲爱的你，情人节快乐' });
-        slides.push({ image: './assets/img/aizaishenqiu.jpg', text: '亲爱的你，情人节快乐' });
-      });
 })(angular)
